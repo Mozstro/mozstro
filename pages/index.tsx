f3cbfa73fa439bbc011b8d2ba60191d4e1ebc80e
Elliot <hello@elliotmorris.dev>
2021-10-25 23:35:01 +0100

    fix blog posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -177,14 +177,14 @@ export default function Home({
                 </li>
               </ul>
               <p className="text-center mt-4 text-lg">
-                Project accomplishments can be found
+                Further details on some of these projects can be found
                 <a
                   className="text-sundance hover:text-ebonyclay cursor-pointer"
                   href="/work"
                 >
                  &nbsp;here
                 </a>
-                .
+                !
               </p>
               <h3 className="text-center text-ebonyclay mb-3 mt-16 lg:mt-24 text-5xl">
                 Get in touch!

cd516dfb4bc613031a351745676b101fc945bf69
Elliot <hello@elliotmorris.dev>
2021-10-27 21:48:25 +0100

    remove tags

--- components/meta.tsx
+++ components/meta.tsx
@@ -22,11 +22,9 @@ const Meta = ({ customMeta }: { customMeta?: MetaProps }): JSX.Element => {
       <meta property="og:type" content={meta.type} />
       <meta property="og:site_name" content="Mozstro - Secure cloud consulting" />
       <meta property="og:description" content={meta.description} />
-      <meta property="og:title" content={meta.title} />
       <meta property="og:image" content={meta.image} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@mozstro_" />
-      <meta name="twitter:title" content={meta.title} />
       <meta name="twitter:description" content={meta.description} />
       <meta name="twitter:image" content={meta.image} />
       <link rel="icon" href="/favicon.ico" />

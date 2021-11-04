7ca0d24b59cd572af7357cd5791684dbe9dea002
Elliot <hello@elliotmorris.dev>
2021-11-04 22:26:11 +0000

    500 page

---
+++ pages/500.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NextSeo } from 'next-seo';
+import Layout from '../components/layout';
+import { Header } from '../components/header';
+import { SectionText } from '../components/sectionText';
+
+export default function Custom500() {
+  return (
+    <Layout>
+      <NextSeo
+        title="Mozstro - Internal server error"
+      />
+      <Header>
+        <h1 className="text-4xl lg:text-7xl  text-center text-ebonyclay">
+          Internal server error.
+        </h1>
+        <h2 className="text-center text-ebonyclay text-3xl">
+          Oops this is embarrassing. We will be back soon.
+        </h2>
+      </Header>
+      <section>
+        <div className="flex my-4 justify-center">
+          <SectionText text="500." />
+        </div>
+      </section>
+    </Layout>
+  );
+}
